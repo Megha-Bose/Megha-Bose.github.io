a6eb670e00947e8c9f8e0c5f66df027b89997bef
Megha-Bose <megh.bose@gmail.com>
2021-01-02 22:31:13 +0530

    Updates

---
+++ js/main.js
@@ -0,0 +1,25 @@
+function readMore() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("about-read-more");
+    var btnText = document.getElementById("myBtn");
+
+    if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btnText.innerHTML = "Read more"; 
+        moreText.style.display = "none";
+    } else {
+        dots.style.display = "none";
+        btnText.innerHTML = "Read less"; 
+        moreText.style.display = "inline";
+    }
+}
+
+$('div.dropdown-menu').on('click', function(event){
+    event.stopPropagation();
+});
+
+$('.dropdown.keep-open').on({
+    "shown.bs.dropdown": function() { this.closable = false; },
+    "click":             function() { this.closable = true; },
+    "hide.bs.dropdown":  function() { return this.closable; }
+});
